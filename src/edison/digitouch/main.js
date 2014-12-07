c0674e00cc15f15fa7cb7dc11806e55f63b0b905
mark <git@nohorse.com>
2014-12-07 15:15:44 -0800

    updates to main

--- src/edison/digitouch/main.js
+++ src/edison/digitouch/main.js
@@ -1,56 +1,3 @@
-/*jslint node:true, vars:true, bitwise:true, unparam:true */
-/*jshint unused:true */
-/*global */
-
-/*
-A simple node.js application intended to read data from Digital pins on the Intel based development boards such as the Intel(R) Galileo and Edison with Arduino breakout board.
-
-MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
-Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
-
-Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
-Using a ssh client: 
-1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
-2. opkg update
-3. opkg upgrade
-
-Article: https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-nodejs-templates
-*/
-
-var mraa = require('mraa'); //require mraa
-//var req = require('request');
-console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
-
-var myDigitalPin6 = new mraa.Gpio(6); //setup digital read on Digital pin #6 (D6)
-myDigitalPin6.dir(mraa.DIR_IN); //set the gpio direction to input
-var current = myDigitalPin6.read();
-
-periodicActivity(); //call the periodicActivity function
-
-function periodicActivity() //
-{
-  var myDigitalValue =  myDigitalPin6.read(); //read the digital value of the pin
-  console.log('Gpio is ' + myDigitalValue); //write the read value out to the console
-    if(myDigitalValue != current){
-        current = myDigitalValue;
-        console.log('value changed!'); //write the read value out to the console
-        var req = require('request');
-        req.post('https://smartstroller.azure-mobile.net/tables/todoitem/',
-            { json: { itemtype : 'touch', text:current } },
-            function (error, response, body) {
-                console.log(body)
-                if (!error && response.statusCode == 200) {
-                    console.log(body)
-                }
-            }
-        );
-        console.log('posted!'); //write the read value out to the console
-    }
-  setTimeout(periodicActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
-}
-
-
-
 
 
 /* ----------------------------------------------------
@@ -85,6 +32,15 @@ var mraa = require("mraa"); // Must have this, provides interface to GPIO pins
     var right_turn_led_value = 0;
     var solenoid_value = 0;
 
+    var old_left_touch_sensor_value = 0;
+    var old_right_touch_sensor_value = 0;
+    var old_left_turn_sensor_value = 0;
+    var old_right_turn_sensor_value = 0;
+
+    var old_left_turn_led_value = 0;
+    var old_right_turn_led_value = 0;
+    var old_solenoid_value = 0;
+
     // ----- Call function defined below forever after delay ---------------
     setInterval(function () {
 
@@ -107,6 +63,10 @@ var mraa = require("mraa"); // Must have this, provides interface to GPIO pins
             solenoid_value = 0;
         }
         solenoid_pin.write(solenoid_value);
+        if(old_solenoid_value!=solenoid_value){
+            old_solenoid_value=solenoid_value;
+            SaveToCloud('break',solenoid_value);
+        }
         
         
         // ----------- Process turn signal sensors ------------------
@@ -135,7 +95,16 @@ var mraa = require("mraa"); // Must have this, provides interface to GPIO pins
             right_turn_led_value = 0;
         }
         left_turn_led_pin.write(left_turn_led_value);
+        if(old_left_turn_led_value!=left_turn_led_value){
+            old_left_turn_led_value=left_turn_led_value;
+            SaveToCloud('left',left_turn_led_value);
+        }
         right_turn_led_pin.write(right_turn_led_value);
+        if(old_right_turn_led_value!=right_turn_led_value){
+            old_right_turn_led_value=right_turn_led_value;
+            SaveToCloud('right',left_turn_led_value);
+        }
+        
         
     }, 500);
 
@@ -158,4 +127,19 @@ function setupIO()
     left_turn_led_pin.write(0);
     right_turn_led_pin.write(0);
 }
+
+function SaveToCloud(itemType, newValue){
+        var req = require('request');
+        req.post('https://smartstroller.azure-mobile.net/tables/todoitem/',
+            { json: { itemtype : itemType, text:newValue } },
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body)
+                }
+            }
+        );
+    }
+}
+
+
 

cdb97b8c88d2e62b3231935d72a67edca899fdac
zeronanoone <contact@joshsackos.com>
2014-12-06 21:04:05 -0800

    added main control code

--- src/edison/digitouch/main.js
+++ src/edison/digitouch/main.js
@@ -48,3 +48,114 @@ function periodicActivity() //
     }
   setTimeout(periodicActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
 }
+
+
+
+
+
+/* ----------------------------------------------------
+                     Smart Stroller     
+   ----------------------------------------------------
+*/
+var mraa = require("mraa"); // Must have this, provides interface to GPIO pins
+
+// ----------------------------
+//         Global Vars
+// ----------------------------
+
+    var left_touch_sensor_pin = new mraa.Gpio(2);
+    var right_touch_sensor_pin = new mraa.Gpio(3);
+    var left_turn_sensor_pin = new mraa.Gpio(4);
+    var right_turn_sensor_pin = new mraa.Gpio(5);
+    var left_turn_led_pin = new mraa.Gpio(11);
+    var right_turn_led_pin = new mraa.Gpio(12);
+    var solenoid_pin = new mraa.Gpio(13);
+
+// ---------------------------------------------------------------
+//                     Application Entry Point
+// ---------------------------------------------------------------
+
+    setupIO();  // Setup pin directions and initial states
+
+    var left_touch_sensor_value = 0;
+    var right_touch_sensor_value = 0;
+    var left_turn_sensor_value = 0;
+    var right_turn_sensor_value = 0;
+    var left_turn_led_value = 0;
+    var right_turn_led_value = 0;
+    var solenoid_value = 0;
+
+    // ----- Call function defined below forever after delay ---------------
+    setInterval(function () {
+
+        // Read sensor data
+        left_touch_sensor_value = left_touch_sensor_pin.read();
+        right_touch_sensor_value = right_touch_sensor_pin.read();
+        left_turn_sensor_value = left_turn_sensor_pin.read();
+        right_turn_sensor_value = right_turn_sensor_pin.read();
+
+        
+        // ----------- Process touch sensors ------------------
+        if(left_touch_sensor_value || right_touch_sensor_value)
+        {
+            // Retract brake
+            solenoid_value = 1;
+        }
+        else
+        {
+            // Extend brake
+            solenoid_value = 0;
+        }
+        solenoid_pin.write(solenoid_value);
+        
+        
+        // ----------- Process turn signal sensors ------------------
+        if(left_turn_sensor_value && right_turn_sensor_value)
+        {
+            // Brake
+            left_turn_led_value = 1;
+            right_turn_led_value = 1;
+        }
+        else if(left_turn_sensor_value && !right_turn_sensor_value)
+        {
+            // Left turn signal
+            left_turn_led_value = ~left_turn_sensor_value;
+            right_turn_led_value = 0;
+        }
+        else if(!left_turn_sensor_value && right_turn_sensor_value)
+        {
+            // Right turn signal
+            left_turn_led_value = 0;
+            right_turn_led_value = ~right_turn_sensor_value;
+        }
+        else
+        {
+            // Turn off both signals
+            left_turn_led_value = 0;
+            right_turn_led_value = 0;
+        }
+        left_turn_led_pin.write(left_turn_led_value);
+        right_turn_led_pin.write(right_turn_led_value);
+        
+    }, 500);
+
+// ---------------------------------------------------------------
+//                    Setup Input/Output Pins
+// ---------------------------------------------------------------
+function setupIO()
+{
+    left_touch_sensor_pin.dir(mraa.DIR_IN);
+    right_touch_sensor_pin.dir(mraa.DIR_IN);
+    left_turn_sensor_pin.dir(mraa.DIR_IN);
+    right_turn_sensor_pin.dir(mraa.DIR_IN);
+    
+    left_turn_led_pin.dir(mraa.DIR_OUT);
+    right_turn_led_pin.dir(mraa.DIR_OUT);
+    solenoid_pin.dir(mraa.DIR_OUT);
+    
+    // Initial values
+    solenoid_pin.write(0);
+    left_turn_led_pin.write(0);
+    right_turn_led_pin.write(0);
+}
+
